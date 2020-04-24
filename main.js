d39ce54163155e0476b0749d1a9e3a80fb57a70d
gp514 <perownee@gmail.com>
2020-04-24 15:27:37 -0400

    full javascript functionality for base use cases

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const bookshelf = document.querySelector(".bookshelf"); 
+const submitForm = document.querySelector("button");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,6 +15,10 @@ function Book(title, author, pages, read) {
     } 
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 function addBookToLibrary(title, author, pages, read) {
     const addedBook = new Book(title, author, pages, read);
     myLibrary.push(addedBook);
@@ -22,36 +29,80 @@ addBookToLibrary("East of Eden", "John Steinbeck", 515, true);
 addBookToLibrary("A Calamitous Mirror", "Barbara Tuchman", 603, false);
 addBookToLibrary("Antifragile", "Nassim Taleb", 355, false);
 
-const bookFrag = document.createDocumentFragment();
 
-for(let i = 0; i < myLibrary.length; i++) {
-    const bookElement = document.createElement("div");
-    bookElement.classList.add(".card");
-    bookElement.innerHTML = `<div class="content">
-    <div class="image">
-       <img id="test" class="ui image tiny right floated" src="media/book-illustration.png">
-   </div> 
-   <div class="header">${myLibrary.title}</div>          
-   <div class="meta">
-       <span class="pages">515</span> pages
-   </div>
-   <div class="author">
-     John Steinbeck
-   </div>
-   <div class="read-status">
-     <em>Not Read</em>
-   </div>
- </div>
- <div class="extra content">
-       <div class="ui two buttons">
-           <div class="ui basic green button">
-               <i class="check icon"></i>
-               Mark Read
-           </div>
-           <div class="ui basic red button">
-                   <i class="trash icon"></i>
-                   Delete
-           </div>
-       </div>
- </div>`
+render();
+
+function render() {
+    bookshelf.innerHTML = "";
+    for(let i = 0; i < myLibrary.length; i++) {
+        createCard(i);
+    }
+}
+
+function createCard(index) {
+
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.setAttribute("data-cardNum", index);
+
+    const title = document.createElement("h2");
+    title.classList.add("title");
+    title.textContent = myLibrary[index].title;
+
+    const author = document.createElement("p");
+    author.classList.add("author");
+    author.textContent = myLibrary[index].author;
+
+    const pages = document.createElement("p");
+    pages.classList.add("pages");
+    pages.textContent = myLibrary[index].pages;
+
+    const readState = document.createElement("p");
+    readState.classList.add("readState");
+    readState.textContent = myLibrary[index].read;
+    readState.addEventListener("click", toggleRead);
+
+    const trash = document.createElement("i");
+    trash.classList.add("fas", "fa-trash");
+    trash.style.float = "right";
+    trash.addEventListener("click", removeBook);
+
+    newCard.append(title, author, pages, readState, trash);
+    bookshelf.appendChild(newCard);
+}
+
+submitForm.addEventListener("click", addBook);
+
+function addBook() {
+    if(!validateInput(document.forms["bookInput"]["title"].value, document.forms["bookInput"]["author"].value, document.forms["bookInput"]["pages"].value)) {
+        return false;
+    }
+    addBookToLibrary(document.forms["bookInput"]["title"].value, document.forms["bookInput"]["author"].value, document.forms["bookInput"]["pages"].value, document.forms["bookInput"]["readState"].value);  
+    render();
+    clearForm();
+}
+
+// remove book from array based on book that was clicked and re-render cards
+function removeBook(event) {
+    myLibrary.splice(event.path[1].getAttribute("data-cardNum"), 1);
+    render();
+}
+
+function toggleRead(event) {
+    myLibrary[event.path[1].getAttribute("data-cardNum")].toggleRead();
+    render();
+}
+
+function validateInput(...args) {
+    for(let i = 0; i < args.length; i++) {
+        if(!args[i]) return false
+    }
+    return true;
+}
+
+function clearForm() {
+    document.forms["bookInput"]["title"].value = "";
+    document.forms["bookInput"]["author"].value = "";
+    document.forms["bookInput"]["pages"].value = null;
+    document.forms["bookInput"]["readState"].checked = false;
 }
